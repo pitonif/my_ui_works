1d33c36c0505aa0638a915af32362a7a89fe16c1
Lobachkov Oleg <olobachkov@gmail.com>
2022-02-16 15:14:46 +0200

    create cell

---
+++ UITableViewController Static Table/UITableViewController/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  UITableViewController
+//
+//  Created by Oleg on 15.02.2022.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
