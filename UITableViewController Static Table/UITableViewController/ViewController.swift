0a5038a04c3aa1d1b04a7349c4d42af1bdd44c20
Lobachkov Oleg <olobachkov@gmail.com>
2022-02-16 15:16:39 +0200

    add table view controller static

---
+++ UITableViewController Static Table/UITableViewController/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UITableViewController
+//
+//  Created by Oleg on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
