f1822d2ef04d71cf4bce3d1dc1c048c8a8381c09
Lobachkov Oleg <olobachkov@gmail.com>
2022-02-17 13:12:50 +0200

    create project

---
+++ UICollectionView/UICollectionView/MenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuViewController.swift
+//  UICollectionView
+//
+//  Created by Oleg on 17.02.2022.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+
+}
---
+++ UICollectionView/UICollectionView/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  UICollectionView
+//
+//  Created by Oleg on 17.02.2022.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
